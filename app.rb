279063a792c0ee4dddc95836c6e690ce47a7d08d
Henning Vogt <henning@vogt.family>
2021-02-19 23:46:10 +0100

    Always require all workers/jobs

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 require 'sidekiq'
 require 'sidekiq/web'
 require 'config/boot'
-require 'app/jobs/test_worker'
+Dir["app/jobs/*.rb"].each {|file| require file }
 
 # Nothing to do here
 class App; end
